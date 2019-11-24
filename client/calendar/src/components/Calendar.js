cda586df14436a65ba161ff86fe6a616a1809efa
AhmedAbdel-Aal <a7med_201196@yahoo.com>
2019-11-25 00:48:57 +0200

    fetch deadlines from mongo

--- client/calendar/src/components/Calendar.js
+++ client/calendar/src/components/Calendar.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import axios from 'axios'
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import timeGridPlugin from '@fullcalendar/timegrid'
 import interactionPlugin from '@fullcalendar/interaction' // needed for dayClick
-
+import Loader from './Loader'
 import './calendar.scss'
+
+var moment = require('moment');
 
 export default class Calendar extends React.Component {
 
@@ -13,34 +16,63 @@ export default class Calendar extends React.Component {
         this.state = {
             calendarWeekends: true,
             calendarEvents: [ // initial event data
-                { title: 'Event Now Now', start: new Date() }
-            ]
+                { title: 'Event', start: '2019-11-01' },
+            ],
+            loading:false,
         };
     }
 
-  calendarComponentRef = React.createRef()
+    componentDidMount=async()=>{
+        await axios.get("/api/deadline",
+        (       'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept'
+      ))
+          .then(res =>  res.data)
+          .then(json =>{
+            if(json.data){
+                console.log(json.data)
+                let arr = []
+                arr = this.state.calendarEvents
+                json.data.map(e =>{
+                    arr.push({title:e.name,start:moment(e.deadline).format().substr(0, 10)})
 
+                })
+                this.setState({
+                    calendarEvents : arr,
+                    loading : true
+                })
+           }})
+           .catch(error => {  this.setState({error})} );
+    }
+
+  calendarComponentRef = React.createRef()
+ 
   render() {
     console.log(this.state.calendarEvents)  
-    return (
-      <div className='demo-app'>
-        <div className='demo-app-calendar'>
-          <FullCalendar
-            defaultView="dayGridMonth"
-            header={{
-              left: 'prev,next today',
-              center: 'title',
-              right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
-            }}
-            plugins={[ dayGridPlugin, timeGridPlugin, interactionPlugin ]}
-            ref={ this.calendarComponentRef }
-            weekends={ this.state.calendarWeekends }
-            events={ this.state.calendarEvents }
-            dateClick={ this.handleDateClick }
-            />
+    if(this.state.loading){
+        return (
+        <div className='demo-app'>
+            <div className='demo-app-calendar'>
+            <FullCalendar
+                defaultView="dayGridMonth"
+                header={{
+                left: 'prev,next today',
+                center: 'title',
+                right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
+                }}
+                plugins={[ dayGridPlugin, timeGridPlugin, interactionPlugin ]}
+                events={ this.state.calendarEvents }
+                dateClick={ this.handleDateClick }
+                />
+            </div>
         </div>
-      </div>
-    )
+        )
+    }
+    else{
+        return(
+            <Loader></Loader>
+        )
+    }
   }
 
 
